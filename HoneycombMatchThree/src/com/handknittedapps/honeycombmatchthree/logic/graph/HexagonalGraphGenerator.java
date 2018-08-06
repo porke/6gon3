3111106e93938c796a7f02f0669b67f6f36e413f
Wojciech Czabański <porke@gazeta.pl>
2018-08-06 14:59:28 +0200

    Update HexagonalGraphGenerator.java
    Revert update

--- HoneycombMatchThree/src/com/handknittedapps/honeycombmatchthree/logic/graph/HexagonalGraphGenerator.java
+++ HoneycombMatchThree/src/com/handknittedapps/honeycombmatchthree/logic/graph/HexagonalGraphGenerator.java
@@ -10,7 +10,107 @@ public class HexagonalGraphGenerator implements GraphGenerator<Block>
 	@Override
 	public void generateGraph(Graph<Block> graph, int sideSize)
 	{
-	
+		this.gridSize = sideSize;
+		this.graph = graph;
+
+		int numTiles = this.gridSize * this.gridSize;
+		for (int i = 0; i < numTiles; ++i)
+		{
+			GraphVertex<Block> curr = this.graph.getVertex(i);
+			Point blockPos = curr.getData().getPosition();
+
+			//First row
+			if (blockPos.y == 0)
+			{
+				//Bottom
+				if (blockPos.x == 0)
+				{
+					this.addLink(i, 0);
+					this.addLink(i, 1);
+				}
+				//Top
+				else if (blockPos.x == this.gridSize - 1)
+				{
+					this.addLink(i, 0);
+					this.addLink(i, 4);
+					this.addLink(i, 5);
+				}
+				//The rest apart from the top and bottom elements
+				else
+				{
+					if (blockPos.x % 2 == 1)
+					{
+						this.addLink(i, 2);
+						this.addLink(i, 4);
+					}
+					this.addLink(i, 0);
+					this.addLink(i, 1);
+					this.addLink(i, 5);
+				}
+			}
+			// Top row
+			else if (blockPos.y == this.gridSize - 1)
+			{
+				//First element
+				if (blockPos.x == 0)
+				{
+					this.addLink(i, 1);
+					this.addLink(i, 2);
+					this.addLink(i, 3);
+				}
+				//Last element
+				else if (blockPos.x == this.gridSize - 1)
+				{
+					if (blockPos.x % 2 == 0)
+					{
+						this.addLink(i, 5);
+					}
+
+					this.addLink(i, 3);
+					this.addLink(i, 4);
+				}
+				//The rest apart from the first and last element
+				else
+				{
+					if (blockPos.x % 2 == 0)
+					{
+						this.addLink(i, 1);
+						this.addLink(i, 5);
+					}
+					this.addLink(i, 2);
+					this.addLink(i, 3);
+					this.addLink(i, 4);
+				}
+			}
+			//The middle elements
+			else
+			{
+				//First column
+				if (blockPos.x == 0)
+				{
+					this.addLink(i, 0);
+					this.addLink(i, 1);
+					this.addLink(i, 2);
+					this.addLink(i, 3);
+				}
+				//Last column
+				else if (blockPos.x == this.gridSize - 1)
+				{
+					this.addLink(i, 0);
+					this.addLink(i, 3);
+					this.addLink(i, 4);
+					this.addLink(i, 5);
+				}
+				//All other fields
+				else
+				{
+					for (int d = 0; d < HexagonalGraphGenerator.NumDirections; ++d)
+					{
+						this.addLink(i, d);
+					}
+				}
+			}
+		}
 	}
 
 	private void addLink(int currIdx, int direction)
